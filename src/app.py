60abc6ff1f51f3cbdb7e9646a0827eb7b4aa889b
felagorn <69029805+felagorn@users.noreply.github.com>
2021-04-27 21:21:51 +0700

    Update app.py
    3 sql statements

--- src/app.py
+++ src/app.py
@@ -1,6 +1,33 @@
 import re
 from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 import sqlite3
+import datetime
+
+def sqlConnect(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+    except sqlite3.Error as e:
+        print(str(e))
+    return connection
+
+def sqlUpdate(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except sqlite3.Error as e:
+        print(str(e))
+
+def sqlSelect(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+    except sqlite3.Error as e:
+        print(str(e))
+    return result
 
 app = Flask(__name__)
 app.config["UPLOAD_PATH"] = "../test"
