4ec43c630fee6759d72bca21220bf92bf8357628
felagorn <69029805+felagorn@users.noreply.github.com>
2021-04-28 12:14:05 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,39 +3,14 @@ import re
 from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 import sqlite3
 import datetime
-
-def sqlConnect(path):
-    connection = None
-    try:
-        connection = sqlite3.connect(path)
-    except sqlite3.Error as e:
-        print(str(e))
-    return connection
-
-def sqlUpdate(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        connection.commit()
-    except sqlite3.Error as e:
-        print(str(e))
-
-def sqlSelect(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-    except sqlite3.Error as e:
-        print(str(e))
-    return result
+from flask_sqlalchemy import SQLAlchemy
 
 def regexTanggal(s):
     tanggals = re.findall('(\\b\\d{2}/([1-9]|1[0-2]|0[1-9])/\\d{4}\\b)', s)
     tanggal =[]
     for t in tanggals: 
         tanggal.append(t[0])
-    print(tanggal)
+    return tanggal
 
 def regexKataPenting(s):
     katapenting = []
@@ -84,6 +59,8 @@ def regexFitur(s):
 
 app = Flask(__name__)
 app.config["UPLOAD_PATH"] = "../test"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(app.config["UPLOAD_PATH"], "jadwal.db")
+db = SQLAlchemy(app)
 
 @app.route("/")
 @app.route("/index")
@@ -94,4 +71,6 @@ def index():
 def chat():
     query = request.args.get("q")
     print(query)
+    tanggal = regexTanggal(query)
+    print(tanggal)
     return render_template("chat.html", query=query)
